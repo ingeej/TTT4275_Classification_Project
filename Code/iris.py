23eef7f31c8f11f8c1c3ed88e51788c92ce46ad6
ingeej <inge.ej@gmail.com>
2021-04-26 11:56:53 +0200

    Added histogram plots

--- Code/iris.py
+++ Code/iris.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 
 
-def importIris(filename):
+
+def importIris(filename,slice1, slice2):
     rawData = np.genfromtxt(filename, dtype=str, delimiter=",") #m*n-matrix, m = features, n = observations
     data = np.empty((3,50,4)) # 3*50-matrix, each column is one class
     indices = [0,0,0] #
@@ -18,8 +19,8 @@ def importIris(filename):
         elif row[len(row)-1] == "Iris-virginica":
             data[2,indices[2]] = row[0:4]
             indices[2] +=1
-    ts = data[:,:30]
-    vs = data[:, 30:50]
+    ts = data[:,:slice1]
+    vs = data[:, slice1:(slice1+slice2)]
     return ts, vs, data
 
 def sigmoid(zik):
@@ -81,7 +82,8 @@ def classifier(W, test_set): # W must be the already trained matrix
         omega = int(np.argmax(np.matmul(W,s.T)))
         #print(np.matmul(W,test_set[4,0,:].T))
     return omega
-def confuMarix(W, test_set):
+
+def confuMatrix(W, test_set):
     classes, samples, features = test_set.shape
     confusion_matrix = np.zeros((classes, classes)) # n*m, n = prediction, m = true
     #Husk! Finn feil som funksjon av alpha
@@ -89,36 +91,75 @@ def confuMarix(W, test_set):
         for s in test_set[flower,:]:
             omega = int(np.argmax(np.matmul(W,s.T)))
             confusion_matrix[omega][flower] += 1
-    print(confusion_matrix)
-"""
-def grad_W_MSE(W,ts): # W = matrix to train, ts = training set
-    #Noe kode som finner g_k og t_k
-    grad =  np.multiply(np.multiply((g_k-t_k),g_k)),(1-g_k))) #elementwise mult
-    return grad
-def trainW(ts, N, alpha): 
-    classes, samples, features = ts.shape
 
-    W = np.zeros((classes, features)) #Initializing untrained W
+    errorRate = (confusion_matrix[0,1]+confusion_matrix[0,2]+confusion_matrix[1,0]+confusion_matrix[1,2]+confusion_matrix[2,0]+confusion_matrix[2,1]) / (confusion_matrix.sum())
+    return confusion_matrix, errorRate
 
-    for i in range(N): # This loop is what does the training of W
-        grad = grad_W_MSE(W, ts)
-        W -= alpha*grad # Do we have to transpose?
-        #Need some function to stop if it takes to much time, 
-        #and to check if alpha is good enough?
-    return W # The trained matrix, which we can use for classification
+def calculateAlpha(start, end, n, ts, trainSize):
+    alphaSet = np.linspace(start, end, n)
+    errorSet = []
 
 
+    for a in alphaSet:
+        W = trainModel(ts,trainSize, a)
 
-"""
+        CF, ER = confuMatrix(W, ts) # Bruker training set, burde vi bruke test?
+        errorSet.append(ER)
+
+    errorSet = np.array(errorSet)
+    alpha = np.argmin(errorSet)
+    
+
+    plt.scatter(alphaSet, errorSet)
+    plt.xlabel("alpha")
+    plt.ylabel("Error rate")
+    plt.show()
+    print(alpha)
+    return errorSet, alpha
+
+def plotHistogram(set, feature):
+    bins = np.arange(0, 8, 1/7)
+    featureNames = ["Sepal length", "Sepal Width", "Petal Length", "Petal Width"]
+    fig, axs = plt.subplots(3,1)
+    for (i, ax) in enumerate(axs.flat):
+        ax.hist(set[i,:,feature].tolist(),bins=bins)
+        ax.set_ylabel("Class %d" %(i))
+        #ax.set_ylim((0,13))
+    plt.xlabel("%s [cm]" % (featureNames[feature]))
+    plt.show()
+       
+        
+
+
 #task 1a
-ts, vs, data = importIris('Kode\iris\iris.data')
+ts1, vs1, data1 = importIris('Code\iris\iris.data', 30, 20)
 print("Task 1a)")
-print("Training samples:",ts.shape)
-print("Verification samples:",vs.shape)
+print("Training samples:",ts1.shape)
+print("Verification samples:",vs1.shape)
 
-#task 2a
-#print(ts[:,0]) #3x4
-#print(ts[:,1])
-W = trainModel(ts, 1000, 0.0092)
-print(W) 
-confuMarix(W,data) 
+#1b
+#calculateAlpha(0.01,0.1,30,ts,1000) # valgt 0.009
+#calculateAlpha(0.001,0.02,30,ts,1000) # valgt 0.009
+"""
+W1 = trainModel(ts1, 1000, 0.009)
+CMts1, ERts1 = confuMatrix(W1,ts1) 
+CMvs1, ERvs1 = confuMatrix(W1,vs1) 
+print("ts = 30, vs = 20, ts used for training")
+print("Training set error rate: ", ERts1, "\n",CMts1)
+print("Verification set error rate: ", ERvs1, "\n",CMvs1)
+
+#1d
+ts2, vs2, data2 = importIris('Code\iris\iris.data', 20, 30)
+
+W2 = trainModel(vs2, 1000, 0.009)
+CMts2, ERts2 = confuMatrix(W2,ts2) 
+CMvs2, ERvs2 = confuMatrix(W2,vs2) 
+print("\nts = 20, vs = 30, vs used for training")
+print("Training set error rate: ", ERts2, "\n",CMts2)
+print("Verification set error rate: ", ERvs2, "\n",CMvs2)
+"""
+#2a
+plotHistogram(data1, 0)
+plotHistogram(data1, 1)
+plotHistogram(data1, 2)
+plotHistogram(data1, 3)

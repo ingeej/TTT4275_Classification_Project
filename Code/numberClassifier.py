7bc00ec5cf04598f00d5b267eaeb745fdcce3f31
ingeej <inge.ej@gmail.com>
2021-04-28 17:35:25 +0200

    clustering and filesaving finished

--- Code/numberClassifier.py
+++ Code/numberClassifier.py
@@ -2,8 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
+from sklearn.cluster import KMeans
 import time
-
+from scipy import stats
 from multiprocessing import Process
 t1 = time.time()
 
@@ -12,7 +13,6 @@ t1 = time.time()
 def importBIN():
     nSamples = 60000
     nTests = 1000
-    #nClasses = 10
 
     with open('Code/mnist/train_images.bin','rb') as binaryFile:
         imgB = binaryFile.read()
@@ -50,10 +50,10 @@ def eucledianDistance(img1, img2):
 def NNpredictor(test, ref, refLables, trueLables):  # No need for teslabels (not without clustering)
 
     predictedLables = []
-    #i = 0
+    i = 0
     for testPicture in test:
         distance = []  # Distance array, from testpic to all refpics
-        #correctLable = trueLables[i]
+        correctLable = trueLables[i]
         for refPicture in ref:
             distance.append(eucledianDistance(refPicture, testPicture))
        
@@ -62,22 +62,25 @@ def NNpredictor(test, ref, refLables, trueLables):  # No need for teslabels (not
         """
         if correctLable != testLable:
             print("Correct label", correctLable, " predicted lable: ", testLable)
+            print(eucledianDistance(testPicture, ref[np.argmin(distance)]))
             printNumber(testPicture)
             printNumber(ref[np.argmin(distance)])
-            printNumber(plotDiffPics(testPicture,ref[np.argmin(distance)]))
-        
-        
+            printNumber(differenceImage(testPicture,ref[np.argmin(distance)]))
+
         i+=1
         """
     return predictedLables
 
 
-def confusionMatrix(test, trueLables, ref, refLables):
-    predictedLables = NNpredictor(test, ref, refLables, trueLables)
+def confusionMatrix(test, testLables, ref, refLables, k=0):
+    if k==0:
+        predictedLables = NNpredictor(test, ref, refLables, testLables)
+    else:
+        predictedLables = KNN(test, ref, testLables, refLables, k)
     #print("actual label: %s,\n   predicted: %s \n" % (trueLables, np.array(predictedLables)))
     confuMatrix = np.zeros((10, 10))
     for i in range(len(predictedLables)):
-        confuMatrix[predictedLables[i], trueLables[i]] += 1
+        confuMatrix[predictedLables[i], testLables[i]] += 1
     correct = 0
     for el in range(10):
         correct += confuMatrix[el][el]
@@ -86,36 +89,102 @@ def confusionMatrix(test, trueLables, ref, refLables):
     return confuMatrix, errorRate
 
 
-def plotDiffPics(img1, img2):
-    #tempArr = np.zeros((28,28))
-    temp1 = differenceImage(img1, img2)
-    #printNumber(temp1)
-    #diffPic = np.sqrt(np.square(temp1))
-    #diffPic = np.multiply((img1 - img2).T, (img1- img2))
-    return temp1
 
 def differenceImage(img1, img2):
-    a = img1-img2
-    b = np.uint8(img1<img2) * 254 + 1
-    return a * b
+    posDiff = img1-img2 # Since we import uint8, this will give zero for img1[i]-img2[i] < 0
+    negDiff = np.uint8(img1<img2) * 254 + 1 # Making array with
+    #negDiff = img2-img1
+    #return posDiff + negDiff 
+    return posDiff * negDiff
+
+#def sort()
+
+
+
+def getClusters(ref, refLables, nClusters=64):
+    
+    #clusters = [] # [(label, kmeans[])] # 10*64*28*28
+    clusterData = [] # [kmeans] 10*64*28*28
+    classes = range(len(np.unique(refLables))) #creates a list [0:9]
+
+    for c in classes: # For every number 0-9
+        i = np.where(refLables == c)[0] #indices for the number
+        data = ref[i] # All pictures of number c
+        data = data.reshape((data.shape[0], -1)) #make it 1d
+        kmeans = KMeans(n_clusters=nClusters, n_init=20, n_jobs=1) # mean-picture
+
+        kmeans.fit_predict(data) # cluster centers and index 
+        cluster = kmeans.cluster_centers_ #
+        cluster = cluster.reshape((64,28,28))
+        clusterData.extend(cluster)
+
+    clusterData = np.array(clusterData)
+    clusterData = np.uint8(clusterData)
+    #create a 10*64 array with 0-9
+
+    clusterLables = []
+    for c in range(10):
+        clusterLables.extend([c]*nClusters)
+    clusterLables = np.array(clusterLables)
+
+    return clusterData, clusterLables
+
+
+def KNN(test, ref, testLables, refLables, k):
+    predictedLables = []
+    for testPicture in test:
+        distance = []  # Distance array, from testpic to all refpics
+        #correctLable = trueLables[i]
+        for refPicture in ref:
+            distance.append(eucledianDistance(refPicture, testPicture)) #distances
+        knnIndex = np.argpartition(distance,k)[:k] # List with index to k lowest distances
+        knnValue = np.take(refLables, knnIndex)
+        #print(stats.mode(knnValue))
+        predictedLables.append(np.bincount(knnValue).argmax())  # Mode of KNN-list
+    return predictedLables
+
+#Saves 3d array to file
+
+def saveFile(arr, name):
+    np.save(name, arr)
+
+def loadFile(name):
+    loaded_arr = np.load(name)
+    return loaded_arr
 
 #Load dataset (60000x28x28) 
 (train_x, train_y), (test_x, test_y) = mnist.load_data()
 #train_x, train_y, test_x, test_y = importBIN()
-N = 1000 #number of pictures we want to use in each training 
-M = 1
 
-conMatrix, ER= confusionMatrix(test_x[:10], test_y[:10],  train_x, train_y)
-print(conMatrix, "\n ER: ", ER)
+#task 1
+#conMatrix, ER= confusionMatrix(test_x[:10], test_y[:10],  train_x, train_y,0)
+#print(conMatrix, "\n ER: ", ER)
 
-"""
-imgblack = np.zeros((28,28))
-imgWhite = np.full((28,28),255.0)
-imgWhite[5][5] = 0
-imgblack[10][10] = 255
+#task 2
 
-printNumber(plotDiffPics(imgWhite, imgblack))
-"""
+N = 6000 # Number of pictures we want to use in each training 
+M = 10 # Number of classes
+nClusters = 64 # Number of clusters for each class
+
+#clusterData, clusterLables = getClusters(train_x, train_y, nClusters)
+
+clusterData = loadFile('clusterData.npy')
+clusterLables = loadFile('clusterLables.npy')
+
+
+
+
+conMatrix, ER= confusionMatrix(test_x, test_y,  clusterData, clusterLables,0)
+print(conMatrix)
+print("Error",ER)
+
+
+#task 2c 
+
+#conMatrix, ER= confusionMatrix(test_x[10:30], test_y[10:30],  train_x, train_y,7)
+#print(conMatrix, "\n ER: ", ER)
+
 t2 = time.time()
 t = t2 - t1
 print("Runtime: %.20f" % t)
+#saveFile(clusterLables, 'clusterLables.npy')
